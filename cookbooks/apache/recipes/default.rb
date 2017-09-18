59ef4209f4291d9e78180154c4819bc2f5e501e8
harish <harishsheshadri1893@gmail.com>
2017-09-18 15:36:02 +0530

    package resource added to default.rb of apache server

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -6,3 +6,12 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+package 'Install Apache' do
+  case node[:platform]
+  when 'redhat', 'centos'
+    package_name 'httpd'
+  when 'ubuntu', 'debian'
+    package_name 'apache2'
+  end
+end
